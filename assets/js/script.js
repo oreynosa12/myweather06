063be665184466e21937274744e64ef258d481f8
OMR K9 <121472588+oreynosa12@users.noreply.github.com>
2023-03-04 13:19:33 -0500

    added background image and bootstrap and some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,6 @@
-// fetch(api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={db61d99869c29302a76214785fc276c2})
-// .then(function(data){
-//     return(data.json())
-// })
 
-fetch(
-  `https://api.tomtom.com/search/2/search/fuel%20depot.json?key=NqrO4nkUsqVudnvjKTmTAqREWXlkjdnv&lat=37.8085&lon=-122.4239&radius=10000`
-)
+fetch(api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={db61d99869c29302a76214785fc276c2}
+  
   .then(function (data) {
     return data.json();
   })
