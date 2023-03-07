3405b7e3ed367fa58eee9151763f871f348e2789
OMR K9 <121472588+oreynosa12@users.noreply.github.com>
2023-03-06 22:35:59 -0500

    added html cards and code for forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,42 +8,58 @@ function currentWeather(city) {
       return data.json();
     })
     .then(function (data) {
-      console.log(data);
-      $(".card h3").text(`Weather in ${data.name}`);
-      $(".temp").text(`Temperature: ${data.main.temp} degrees`);
-      $(".description").text(`${data.weather[0].description}`);
-      $(".humidity").text(`Humidity: ${data.main.humidity}`);
-      $(".wind").text(`Wind speed: ${data.wind.speed}mph`);
-      var img = $("img");
+      //   console.log(data);
+      $("#current  h3").text(`Weather in ${data.name}`);
+      $("#current .temp").text(`Temperature: ${data.main.temp} degrees`);
+      $("#current .description").text(`${data.weather[0].description}`);
+      $("#current .humidity").text(`Humidity: ${data.main.humidity}`);
+      $("#current .wind").text(`Wind speed: ${data.wind.speed}mph`);
+      var img = $("<img>");
       img.attr(
         "src",
         "https://api.openweathermap.org/img/w/" + data.weather[0].icon + ".png"
       );
-      $(".weather").append(img);
-      forecast(city);
+      $("#current .weather").append(img);
+      fiveForecast(city);
     });
 }
 
-function forecast(city) {
+function fiveForecast(cities) {
   fetch(
-    `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${weather}`
+    `http://api.openweathermap.org/data/2.5/forecast?q=${cities}&units=imperial&appid=${weather}`
   )
     .then(function (data) {
       return data.json();
     })
     .then(function (data) {
-      console.log(data);
-      // $(".card h3").text(`Weather in ${data.name}`);
-      // $(".temp").text(`Temperature: ${data.main.temp} degrees`);
-      // $(".description").text(`${data.weather[0].description}`);
-      // $(".humidity").text(`Humidity: ${data.main.humidity}`);
-      // $(".wind").text(`Wind speed: ${data.wind.speed}mph`);
-      // var img = $("img");
-      // img.attr(
-      //   "src",
-      //   "https://api.openweathermap.org/img/w/" + data.weather[0].icon + ".png"
-      // );
-      // $(".weather").append(img);
+      console.log(data.list);
+
+      for (let i = 0; i < data.list.length; i += 8) {
+        var currentDay = i / 8 + 1;
+        $("#" + currentDay + " .h3").text(`5 Day Forecast ${data.city.name}`);
+        $("#" + currentDay + " .temp").text(
+          `Temperatures: ${data.list[i].main.temp} degrees`
+        );
+        $("#" + currentDay + " .description").text(
+          `${data.list[i].weather[0].description}`
+        );
+        $("#" + currentDay + " .humidity").text(
+          `Humidity: ${data.list[i].main.humidity}`
+        );
+        $("#" + currentDay + " .wind").text(
+          `Wind speed: ${data.list[i].wind.speed}mph`
+        );
+        var img = $("<img>");
+        img.attr(
+          "src",
+          "https://api.openweathermap.org/img/w/" +
+            data.list[i].weather[0].icon +
+            ".png"
+        );
+        $("#" + currentDay + " .weather").append(img);
+      }
+
+      
     });
 }
 
@@ -52,30 +68,3 @@ $("#search").click(function () {
   console.log(city);
   currentWeather(city);
 });
-
-// fetch("https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={db61d99869c29302a76214785fc276c2}")
-
-//   .then(function (data) {
-//     return data.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   });
-
-//   displayWeather : function(data){
-//     const {name} =data;
-//     const {icon, description} = data.weather[0]
-//     const {temp, humidity} = data.main;
-//     const {speed } = data.wind
-//     console.log (name, icon, description, temp, humidity, speed)
-//     document.querySelector(city).innerText = "Weather in" + name;
-//     document.querySelector("icon").src = https://api.openweathermap.org/img/wn/" + icon + .png;
-//     document.querySelector(".description").innerText = description;
-//     document.querySelector(".temp").innerText = temp + "farenheit"
-//     document.querySelector(".wind").innerText = "Wind speed:"+"mi/h";
-//     document.querySelector("weather").classList.remove
-//     document.body.style.backgroundImage = "url('https://image.unsplash.com/1600x900/? + name +')"
-
-//   }
-//   api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-// db61d99869c29302a76214785fc276c2
